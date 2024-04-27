a38a89cbdcc24f4163140544139b49231026b3ae
Jack Dockerty <jdockerty19@gmail.com>
2024-04-27 18:14:12 +0100

    feat: basic axum server

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
+use axum::{
+    http::StatusCode,
+    response::IntoResponse,
+    routing::{get, post},
+    Json, Router,
+};
+use serde::{Deserialize, Serialize};
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    println!("Hello, world!");
-    Ok(())
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new().route("/health", get(health));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await?;
+    tracing::debug!("listening on {}", listener.local_addr()?);
+    Ok(axum::serve(listener, app).await?)
+}
+
+async fn health() -> (StatusCode, &'static str) {
+    (StatusCode::OK, "OK")
 }
